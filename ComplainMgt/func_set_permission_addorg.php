4c59b8b610551fdd524621bf2b024c6a56d36277
Boss <khemmasak@gmail.com>
2023-04-23 13:43:34 +0700

    Edit redirect page auto permission func_set_permission_user.php&pop_set_admission_user.php v2

---
+++ ComplainMgt/func_set_permission_addorg.php
@@ -0,0 +1,29 @@
+<?php
+include("../EWT_ADMIN/comtop_pop.php");
+
+$db->query("USE " . $EWT_DB_USER);
+
+$date = new DateTime();
+
+$a_data = array_merge($_POST, $_FILES);
+$s_data = array();
+
+if ($a_data['proc'] == 'Add') {
+	for ($i = 0; $i < $_POST["alli"]; $i++) {
+		$chk = $_POST["chk" . $i];
+		$uid = $_POST["uid" . $i];
+		if ($chk == "Y") {
+			$sqlchk = $db->query("SELECT COUNT(ugm_id) FROM web_group_member WHERE ug_id = '" . $_POST["ug"] . "' AND ugm_type = 'D' AND ugm_tid = '" . $uid . "' ");
+			$C = $db->db_fetch_row($sqlchk);
+			if ($C[0] == 0) {
+				$db->query("INSERT INTO web_group_member (ug_id,ugm_type,ugm_tid) VALUES ('" . $_POST["ug"] . "','D','" . $uid . "') ");
+			}
+			$s_uid  = $uid;
+		}
+	}
+
+	echo json_encode($s_uid);
+	unset($a_data);
+	unset($s_data);
+	exit;
+}
--- ComplainMgt/menu-top.php
+++ ComplainMgt/menu-top.php
@@ -1,69 +1,61 @@
+
 <div class="row m-b" style="padding-top:65px;">
-        <div class="col-md-12 col-sm-12 col-xs-12 m-b-sm">
-                <div class="card">
-                        <div class="card-body">
-                                <div class="row">
-                                        <div class="col-md-4 col-sm-4 col-xs-12">
-                                                <img src="<?php echo $IMG_PATH; ?>images/webboard.png" class="animated fadeInDown " style="height: 60px;width: 60px;">
-                                                <span class="animated fadeInDown text-large hidden-xs"><?php echo $txt_complain_module; ?></span>
-                                        </div>
+<div class="col-md-12 col-sm-12 col-xs-12 m-b-sm"  >
+<div class="card" >
+<div class="card-body" >
 
-                                        <div class="col-md-8 col-sm-8 col-xs-12 float-right text-right hidden-xs" style="top:18px;">
-                                                <a href="../EWT_ADMIN/main.php">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="fas fa-home"></i>&nbsp;<?php echo $txt_ewt_home; ?>
-                                                        </button>
-                                                </a>
-                                                <a href="complain_main.php" title="<?php echo $txt_complain_menu_main; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo $txt_complain_menu_main; ?>
-                                                        </button>
-                                                </a>
-                                                <a href="complain_group.php" title="<?php echo $txt_complain_menu_cate; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo $txt_complain_menu_cate; ?>
-                                                        </button>
-                                                </a>
-                                                <!-- <a href="complain_form.php" title="<?php echo $txt_complain_menu_form; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo $txt_complain_menu_form; ?>
-                                                        </button>
-                                                </a> -->
-                                                <!-- <a href="complain_page.php" title="<?php echo $txt_complain_menu_form; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo "Complain Add Page"; ?>
-                                                        </button>
-                                                </a> -->
-                                                <!-- <a href="complain_pdpa.php" title="<?php echo 'ข้อกำหนด'; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo "ข้อกำหนด"; ?>
-                                                        </button>
-                                                </a> -->
-                                                <a href="complain_report1.php" title="<?php echo $txt_complain_menu_form; ?>" target="_self">
-                                                        <button type="button" class="btn btn-default btn-sm">
-                                                                <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo "รายงาน"; ?>
-                                                        </button>
-                                                </a>
-                                        </div>
+<div class="row">
 
-                                        <div class="col-md-6 col-sm-6 col-xs-12 float-right text-right  visible-xs row m-b-sm ">
-                                                <div class="btn-group ">
-                                                        <button type="button" data-toggle="dropdown" class="btn btn-default dropdown-toggle"><i class="fas fa-bars"></i> menu <span class="caret"></span></button>
-                                                        <ul class="dropdown-menu dropdown-menu-right">
-                                                                <li><a href="../EWT_ADMIN/main.php"><i class="fas fa-home"></i>&nbsp;<?php echo $txt_ewt_home; ?></a></li>
-                                                                <li><a href="complain_main.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo $txt_complain_menu_main; ?></a></li>
-                                                                <!-- <li><a href="complain_group.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo $txt_complain_menu_cate; ?></a></li> -->
-                                                                <!-- <li><a href="complain_page.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo "Complain Add Page"; ?></a></li> -->
-                                                                <!-- <li><a href="complain_pdpa.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo 'ข้อกำหนด'; ?></a></li> -->
-                                                                <li><a href="complain_report1.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?php echo 'รายงาน'; ?></a></li>
-                                                        </ul>
-                                                </div>
-                                        </div>
+<div class="col-md-4 col-sm-4 col-xs-12" >	
+<img src="<?=$IMG_PATH ;?>images/OrgManager.png" class="animated fadeInDown " style="height: 60px;width: 60px;">  
+<span class="animated fadeInDown text-large hidden-xs"><?=$txt_permission_module ;?></span>
+</div>
+
+<div class="col-md-8 col-sm-8 col-xs-12 float-right text-right hidden-xs" style="top:18px;"> 
+<a href="../EWT_ADMIN/main.php" >
+<button type="button" class="btn btn-default btn-sm" >
+          <i class="fas fa-home"></i>&nbsp;<?=$txt_ewt_home ;?>
+</button>
+</a>
+<a href="permission_main.php" title="<?=$txt_permission_menu_main ;?>"  target="_self">
+<button type="button" class="btn btn-default btn-sm" >
+          <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_main;?>
+</button>
+</a>
+<a href="permission_user.php" title="<?=$txt_permission_menu_user ;?>"  target="_self">
+<button type="button" class="btn btn-default btn-sm" >
+          <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_user;?>
+</button>
+</a>
+<!--<a href="permission_group.php" title="<?=$txt_permission_menu_group;?>"  target="_self">
+<button type="button" class="btn btn-default btn-sm" >
+          <i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_group;?>
+</button>
+</a>-->
+
+<a href="permission_org.php" title="รายการสิทธิ์กลุ่มหน่วยงาน"  target="_self">
+<button type="button" class="btn btn-default btn-sm" >
+          <i class="far fa-arrow-alt-circle-right"></i>&nbsp;รายการสิทธิ์กลุ่มหน่วยงาน
+</button>
+</a>
+
+</div>
+<div class="col-md-6 col-sm-6 col-xs-12 float-right text-right  visible-xs row m-b-sm ">
+<div class="btn-group ">
+        <button type="button" data-toggle="dropdown" class="btn btn-default dropdown-toggle"><i class="fas fa-bars"></i> menu <span class="caret"></span></button>
+        <ul class="dropdown-menu dropdown-menu-right">
+            <li><a href="../EWT_ADMIN/main.php" ><i class="fas fa-home"></i>&nbsp;<?=$txt_ewt_home ;?></a></li>
+            <li><a href="permission_main.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_main;?></a></li>
+			<li><a href="permission_list.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_user;?></a></li>
+			<!--<li><a href="permission_group.php"><i class="far fa-arrow-alt-circle-right"></i>&nbsp;<?=$txt_permission_menu_group;?></a></li>	-->
+		</ul>
+</div>
+</div>
 
 
-                                </div>
+</div>
 
-                        </div>
-                </div>
-        </div>
+</div>
+</div>
+</div>
 </div>
